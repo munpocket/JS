3a429f7a164d4c5a0b6db1c7780e5aa7e275a7c2
munpocket <okazu0409@gmail.com>
2022-08-07 16:37:35 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 
 const pullDownButton = document.getElementById('lists');
-console.log(pullDownButton);
+const pullDownParents = document.getElementById('pull-down');
 
 pullDownButton.addEventListener("mouseover",function(){
-    console.log("乗る");
+    this.setAttribute("style","background-color:blue;")
+    console.log("乗った時は青色");
 })
 
 pullDownButton.addEventListener("mouseout",function(){
-    console.log("外れる");
+    this.removeAttribute("style","background-color:blue;")
+    console.log("外れた時は赤色");
 })
 
 pullDownButton.addEventListener("click",function(){
-    console.log("クリック");
-})
-
-pullDownButton.addEventListener("mouseover",function(){
-    pullDownButton.setAttribute("style","background-color:blue;")
-})
-
-pullDownButton.addEventListener("mouseout",function(){
-    pullDownButton.removeAttribute("style","background-color:blue;")
+    this.setAttribute("style","background-color:green;")
 })
 
 pullDownButton.addEventListener("click",function(){
-    pullDownButton.setAttribute("style","background-color:green;")
+    if(pullDownParents.getAttribute("style") == "display:block;"){
+        pullDownParents.removeAttribute("style","display:block;")
+        console.log("非表示");
+    }else{
+        pullDownParents.setAttribute("style","display:block;")  
+        console.log("表示");
+    }
+    
+    
 })
