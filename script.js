6b98f4d9e3e3b726ba285b4e0879a7cf564a4d35
munpocket <okazu0409@gmail.com>
2022-08-07 15:08:43 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-let lists=document.getElementsByClassName("pull-down-list");
-console.log(lists);
+
+const pullDownButton = document.getElementById('lists');
+console.log(pullDownButton);
+
+pullDownButton.addEventListener("mouseover",function(){
+    console.log("乗る");
+})
+
+pullDownButton.addEventListener("mouseout",function(){
+    console.log("外れる");
+})
+
+pullDownButton.addEventListener("click",function(){
+    console.log("クリック");
+})
+
